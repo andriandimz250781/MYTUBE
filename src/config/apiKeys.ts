994351d94b4d502bbcb6c8be4bb5e81c081b3015
Andrian Dhie <andrian.dhie@gmail.com>
2025-11-12 07:50:23 +0000

    Ini kenapa bro:
    Error: [ Server ] YouTube API Error: "{\"error\":{\"cod

--- src/config/apiKeys.ts
+++ src/config/apiKeys.ts
@@ -7,9 +7,9 @@
  */
 
 export const YOUTUBE_API_KEYS: string[] = [
-  "AIzaSyADi5bKtKUcyqXNWW2X3IQVge30zNMH_CE_KEY1", // Ganti dengan kunci API YouTube Anda
-  "AIzaSyBfrwG1FRjSNssUoW4v_JLmNd2auhHJd5o_KEY2", // Ganti dengan kunci API YouTube Anda
-  "AIzaSyChkr-1ce8WD1aTpoV4-ofDk0vz-KdZ0p8_KEY3", // Ganti dengan kunci API YouTube Anda
-  "AIzaSyDaEatZfmyn0NoZ0D-mOVPyS0-hgKFdqQg_KEY4", // Ganti dengan kunci API YouTube Anda
-  "AIzaSyBXsPKHfMxOjYuz98Mst_Q9oH8G2Qgr1kU_KEY5", // Ganti dengan kunci API YouTube Anda
+  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_1", // Ganti dengan kunci API YouTube Anda
+  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_2", // Ganti dengan kunci API YouTube Anda
+  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_3", // Ganti dengan kunci API YouTube Anda
+  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_4", // Ganti dengan kunci API YouTube Anda
+  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_5", // Ganti dengan kunci API YouTube Anda
 ];
