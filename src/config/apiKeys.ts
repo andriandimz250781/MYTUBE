d84016e551ffdea48ddef394d1d0ac7f7fad2ea2
Andrian Dhie <andrian.dhie@gmail.com>
2025-11-12 08:24:28 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/config/apiKeys.ts
+++ src/config/apiKeys.ts
@@ -7,9 +7,9 @@
  */
 
 export const YOUTUBE_API_KEYS: string[] = [
-  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_1", // Ganti dengan kunci API YouTube Anda
-  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_2", // Ganti dengan kunci API YouTube Anda
-  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_3", // Ganti dengan kunci API YouTube Anda
-  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_4", // Ganti dengan kunci API YouTube Anda
-  "GANTI_DENGAN_KUNCI_API_YOUTUBE_ANDA_5", // Ganti dengan kunci API YouTube Anda
+  "AIzaSyADi5bKtKUcyqXNWW2X3IQVge30zNMH_CE", // Ganti dengan kunci API YouTube Anda
+  "AIzaSyBfrwG1FRjSNssUoW4v_JLmNd2auhHJd5o", // Ganti dengan kunci API YouTube Anda
+  "AIzaSyChkr-1ce8WD1aTpoV4-ofDk0vz-KdZ0p8", // Ganti dengan kunci API YouTube Anda
+  "AIzaSyDaEatZfmyn0NoZ0D-mOVPyS0-hgKFdqQg", // Ganti dengan kunci API YouTube Anda
+  "AIzaSyBXsPKHfMxOjYuz98Mst_Q9oH8G2Qgr1kU", // Ganti dengan kunci API YouTube Anda
 ];
