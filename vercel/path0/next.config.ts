b77d9fa7e50e686688a5f57aacb8ee5c0b6b2465
Andrian Dhie <andrian.dhie@gmail.com>
2025-11-16 11:18:26 +0000

    Bro, ini saat deploy ke vercel error : 18:16:11.901 Running build in Was

---
+++ vercel/path0/next.config.ts
@@ -0,0 +1,55 @@
+
+import type {NextConfig} from 'next';
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  swSrc: 'src/app/worker.ts',
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
